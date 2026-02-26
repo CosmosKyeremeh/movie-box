7e8df07c65528efe7fac9d55df39035631e6c82d
CosmosKyeremeh <kyeremehcosmos938@gmail.com>
2026-02-26 07:36:35 -0600

    Add configuration file for TMDB API with key and base URLs. Although I need not to but My PC is about to be replaced, I'm afraed I might lose it

---
+++ js/config.js
@@ -0,0 +1,7 @@
+const config = {
+    TMDB_API_KEY: '8f1d563e5a25c5e808a9229d44258385',
+    BASE_URL: 'https://api.themoviedb.org/3',
+    IMAGE_BASE_URL: 'https://image.tmdb.org/t/p'
+};
+
+window.MovieBoxConfig = config;
